d1d298b8c92042a41e2523deb0290f97686333a0
Freddy Perez <fred.perez15@gmail.com>
2023-02-24 23:55:24 +0200

    building simpler likes

--- main.js
+++ main.js
@@ -2,8 +2,33 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const likes = document.querySelectorAll('.like-glyph')
+ likes.forEach((pushLike) => {
+  pushLike.addEventListener("click",() => {
 
+    if (pushLike.textContent === EMPTY_HEART) {
+      mimicServerCall()
+      .then(() => {
+        pushLike.textContent = FULL_HEART
+        pushLike.className = 'activated-heart'
+      })
+      .catch((error) => {
+        const errorMessage = document.getElementById('modal')
+          errorMessage.className = ''
+            errorMessage.textContent = error
+              setTimeout(() => errorMessage.className = 'hidden', 3000)
+      })
+    } else {
+      pushLike.textContent = EMPTY_HEART
+      pushLike.className = 'like-glyph'
+    }
+  })
+  
+ })
+  
+
+ //console.log(likes)
+  
 
 
 
